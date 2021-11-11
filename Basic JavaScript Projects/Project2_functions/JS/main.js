437ea94d005c761c58c5e31c670ccca5be6afcb3
dalton704 <lokuma96@gmail.com>
2021-11-11 10:47:34 -0800

    Added comments to my code

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,12 +1,12 @@
-function displayDate() {
-    document.getElementById("demo").innerHTML= Date();
+function displayDate() { //Function that displays date when the Yes button is clicked
+    document.getElementById("demo").innerHTML= Date(); 
 }
 
-function saidno() {
-    document.getElementById("boring").innerHTML = "I didn't want to tell you anyways!"
+function saidno() { //Function that displays message when the No button is clicked
+    document.getElementById("boring").innerHTML = "I didn't want to tell you anyways!" 
 }
 
-function Breakfast() {
+function Breakfast() { //When the text is clicked it transforms into the food concatenate
     var food = "I had oatmeal";
     food += " for breakfast this morning!";
     document.getElementById("Yum").innerHTML = food
