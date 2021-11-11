e8deeb2114398cf1def4c55a8ddc059ffd08dcb1
dalton704 <lokuma96@gmail.com>
2021-11-11 08:49:06 -0800

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,7 +1,17 @@
-var myAlert = "This is an obnoxious alert box. Click \"Ok\""
+var myAlert = "This is an obnoxious alert box. Click \"Ok\""// THis is the alert box when the website first opens
 
-window.alert(myAlert)
+window.alert(myAlert); //This is triggering the alert bos
 
-var A = "How is your day going today?"
+var A = "How is your day going today?" //Assigned a string to variable A
 
-document.write("Hello, World! " + A)
+document.write("Hello, World! " + A); //Triggering the string with variable A
+
+var B = "Hello, World! " + A //Assigned variable B using a string and variable A
+
+document.write(B); //Triggered variable B to display the string
+
+var Boy = "John", Girl = "Jill", Man = "Bob", Woman = "Margaret", Boss = "Norman" //Assigned many variable in one line
+var Boy = Boy.fontcolor("blue"), Girl = Girl.fontcolor("pink"), Man = Man.fontcolor("green"), Woman = Woman.fontcolor("purple"), Boss = Boss.fontcolor("red") //Gave variable font color
+
+document.write(Man); //Called a variable that was assigned earlier
+document.write(5+6); //Called an expression
