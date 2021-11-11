e2818e24b75d14bd14ba5c3349072f08392c97d9
dalton704 <lokuma96@gmail.com>
2021-11-11 08:14:48 -0800

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,9 @@
-window.alert("Hello, World!")
+var myAlert = "This is an obnoxious alert box. Click \"Okay\""
+
+window.alert(myAlert)
+
+document.write("Hello, World!")
+
+var A = "How is your day going today?"
+
+document.write(A)
