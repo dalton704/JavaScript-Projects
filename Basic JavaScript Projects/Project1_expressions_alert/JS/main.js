ef8e7b21ac8f06630ff285dcd799018f729f5173
dalton704 <lokuma96@gmail.com>
2021-11-11 08:52:51 -0800

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -5,13 +5,17 @@ window.alert(myAlert); //This is triggering the alert bos
 var A = "How is your day going today?" //Assigned a string to variable A
 
 document.write("Hello, World! " + A); //Triggering the string with variable A
+document.write("<br>") //New line
 
 var B = "Hello, World! " + A //Assigned variable B using a string and variable A
 
 document.write(B); //Triggered variable B to display the string
+document.write("<br>") //New line
 
 var Boy = "John", Girl = "Jill", Man = "Bob", Woman = "Margaret", Boss = "Norman" //Assigned many variable in one line
 var Boy = Boy.fontcolor("blue"), Girl = Girl.fontcolor("pink"), Man = Man.fontcolor("green"), Woman = Woman.fontcolor("purple"), Boss = Boss.fontcolor("red") //Gave variable font color
 
 document.write(Man); //Called a variable that was assigned earlier
+document.write(", " + Boy)
+document.write("<br>") //New line
 document.write(5+6); //Called an expression
