d2ad403cbfaad46287ffac8f70d730103308f079
dalton704 <lokuma96@gmail.com>
2021-11-11 10:44:46 -0800

    Added function buttons

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,13 @@
+function displayDate() {
+    document.getElementById("demo").innerHTML= Date();
+}
+
+function saidno() {
+    document.getElementById("boring").innerHTML = "I didn't want to tell you anyways!"
+}
+
+function Breakfast() {
+    var food = "I had oatmeal";
+    food += " for breakfast this morning!";
+    document.getElementById("Yum").innerHTML = food
+}
