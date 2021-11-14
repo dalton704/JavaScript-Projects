455599283617e04e2672bebd09fdef953675bca1
dalton704 <lokuma96@gmail.com>
2021-11-14 08:36:29 -0800

    Added multiply and divide

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -7,3 +7,13 @@ function Subtraction_Function() {
     var subtraction = 66 - 41;
     document.getElementById("Sub").innerHTML = "66 - 41 = " + subtraction;
 }
+
+function Multiply_Function() {
+    var Multi = 34 * 9;
+    document.getElementById("Mult").innerHTML = "34 * 9 = " + Multi;
+}
+
+function Divide_Function() {
+    var divide = 77 / 11;
+    document.getElementById("Div").innerHTML = "77 / 11 = " + divide;
+}
