4dd61ee829272244e71541a2c87d8572d2ea3c4b
dalton704 <lokuma96@gmail.com>
2021-11-14 08:31:14 -0800

    Added SUbtraction

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -2,3 +2,8 @@ function Addition_Function() {
     var addition = 10 + 25;
     document.getElementById("Math").innerHTML = "10 + 25 = " + addition;
 }
+
+function Subtraction_Function() {
+    var subtraction = 66 - 41;
+    document.getElementById("Sub").innerHTML = "66 - 41 = " + subtraction;
+}
