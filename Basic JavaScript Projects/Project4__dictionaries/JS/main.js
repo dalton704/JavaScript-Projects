67aaadb21335c4848a219133a01b0cbfa26f952f
dalton704 <lokuma96@gmail.com>
2021-11-14 10:12:41 -0800

    Update main.js

---
+++ Basic JavaScript Projects/Project4__dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function my_First_Dictionary() {
+    var Animal = {
+        Species: "Cat",
+        Color: "White",
+        Breed: "White Tabby",
+        Age: 7,
+        Sound: "Meow"
+    };
+    document.getElementById("DIctionary").innerHTML = Animal.Color;
+}
