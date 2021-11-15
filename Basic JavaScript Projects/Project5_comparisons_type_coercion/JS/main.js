d03a77a2bb27360a83139fb5e7fa2101dab4199b
dalton704 <lokuma96@gmail.com>
2021-11-15 07:50:29 -0800

    Added content

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,3 +1,21 @@
 document.write("11" + 1);
 document.write(" ")
 document.write(typeof 5)
+document.write(" ")
+document.write("Hello" / 75)
+document.write(" ")
+
+function isNaN_one(){
+    document.getElementById("First").innerHTML = isNaN("Hello, World")
+}
+
+function isNaN_two(){
+document.getElementById("Second").innerHTML = isNaN(65)
+}
+function infine(){
+    document.getElementById("infinity").innerHTML = 3E587
+}
+
+function neg_infine(){
+    document.getElementById("negative").innerHTML = -3E587
+}
