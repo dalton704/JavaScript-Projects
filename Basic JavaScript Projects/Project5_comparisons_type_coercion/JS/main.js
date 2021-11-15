d8a38fc3e3258c5faaa9700192eed6a2ad5e3304
dalton704 <lokuma96@gmail.com>
2021-11-15 08:08:48 -0800

    Update main.js

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -19,3 +19,26 @@ function infine(){
 function neg_infine(){
     document.getElementById("negative").innerHTML = -3E587
 }
+
+document.write(" ")
+document.write(10 > 5)
+document.write(" ")
+document.write(10 <= 6)
+console.log(6 * 2)
+console.log((25 + 2) > (5 * 6))
+document.write(" " + (25==4))
+document.write(" " + (8 == 8))
+
+a = 12
+b = 12
+document.write(" " + (a === b))
+
+c = 13
+document.write(" " + (a === c))
+
+d = "12"
+document.write(" " + (a === d))
+
+e = "Hello"
+document.write(" " + (a === e))
+
