4e1397f446dcada88dd05728f3f285328e9cccd0
dalton704 <lokuma96@gmail.com>
2021-11-15 09:16:26 -0800

    Fixed issues

--- Basic JavaScript Projects/Keywords_function/JS/main.js
+++ Basic JavaScript Projects/Keywords_function/JS/main.js
@@ -8,5 +8,5 @@ var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
 var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
 var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
 function my_Function() {
-    document.getElementById("Keywords_and)Constructors").innerHTML = "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
+    document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
 }
