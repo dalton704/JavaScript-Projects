8de1ea8ae8ec30875c20207b0c097ed6c251357f
dalton704 <lokuma96@gmail.com>
2021-11-17 13:39:28 -0800

    Added comments

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,14 +1,14 @@
-function Call_Loop() {
+function Call_Loop() { //Function that calls a while loop
     var Counter = ""
     var X = 0
-    while (X < 11) {
-        Counter += "<br>" + X;
-        X++
+    while (X < 11) { //a while loop that as long as x is less than 11 will proceded to do instructions below
+        Counter += "<br>" + X; 
+        X++ 
     }
     document.getElementById("Loop").innerHTML = Counter
 }
 
-function Get_Length() {
+function Get_Length() { //Function that Gives you how many charcters are in the written statement
     text = "How are you doing today?"
     length = text.length
     document.getElementById("here").innerHTML = length
