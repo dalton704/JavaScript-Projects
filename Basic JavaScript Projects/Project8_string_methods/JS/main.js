7edfe3226adffdc8a2fdd75a8b57acd543c1762b
dalton704 <lokuma96@gmail.com>
2021-11-17 10:22:13 -0800

    Added comments

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -13,27 +13,27 @@ function Split(){  //Function to slice up part of a sentence
     document.getElementById("Spot").innerHTML = section.toUpperCase(); //Added to uppercase. MAde the slice capitalized
 }
 
-function Searching(){
+function Searching(){ //Function to search the index of yur search condition
     var Le_Text = "The rain is falling from the clouds.";
-    document.getElementById("search").innerHTML = Le_Text.search("f");
+    document.getElementById("search").innerHTML = Le_Text.search("f"); //Searched for F in our srting
 }
 
-function Number_String() {
+function Number_String() { //Function that changes a number to a string
     var x = 72
     document.getElementById("string").innerHTML = x.toString();
 }
 
-function Precision() {
+function Precision() { //Function that maximizes the length of a given value
     var Y = 7865.5628909454521846
-    document.getElementById("rounded").innerHTML = Y.toPrecision(8);
+    document.getElementById("rounded").innerHTML = Y.toPrecision(8); //Maximized the length of my number to 8
 }
 
-function Fixed() {
+function Fixed() { //Function that maximizes how many deimcal points.
     var Z = 7865.5628909454521846
-    document.getElementById("decimals").innerHTML = Z.toFixed(3);
+    document.getElementById("decimals").innerHTML = Z.toFixed(3); //We put the max at 3 decimals for my number
 }
 
-function Value() {
+function Value() { //Function that returns the vaue of a string.
     var R = "Hello, World!"
     document.getElementById("hello").innerHTML = R.valueOf();
 }
